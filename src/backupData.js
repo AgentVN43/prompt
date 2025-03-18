2ccbebab7f2a1d5fdfdc862f854026c63aab9f37
thanhvanba <vanbatrungthanh@gmail.com>
2025-03-18 22:31:02 +0700

    commit tool title

---
+++ src/backupData.js
@@ -0,0 +1,16 @@
+const backupData = {
+    product: ["Bếp điện", "Bếp từ", "Bếp điện từ", "Máy hút mùi"],
+    localtion: ["Hà Đông"],
+    need: ["giá rẻ", "loại nào tốt", "chính hãng", "bền", "đẹp"],
+    price: ["dưới 3 triệu", "từ 5 triệu đến 7 triệu"],
+    people: [
+        "cho sinh viên",
+        "cho gia đình đại gia đình",
+        "cho gia đình nhỏ",
+        "cho gia đình 4 người",
+    ],
+    trend: ["theo xu hướng năm 2025"],
+    promotion: ["khuyến mãi tháng 2"],
+};
+
+export default backupData;
